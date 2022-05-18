25ac066314ec9ab438e7d04987fce11c2359424a
xt2023 <2639695678@qq.com>
2022-05-18 18:17:28 +0800

    完成了登录退出(优化版)

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-import {Form} from "element-ui";
-import {Input} from "element-ui";
-import {FormItem} from "element-ui";
-//导入弹框提示组件
-import {Message} from "element-ui";
+
+import { Button, Form,Input,FormItem,Message} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
